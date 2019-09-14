b826d02f16774c5e4ed571a92810493e5bea5998
Isuru Fernando <isuruf@gmail.com>
2019-09-14 19:46:57 +0000

    Fix deps and tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,16 +3,17 @@ cd sagemath
 git checkout ${PKG_VERSION}
 ln -s $PREFIX local
 
-export SAGE_LOCAL=$PREFIX/local
-export SAGE_ROOT=$PREFIX
+export SAGE_LOCAL=$PWD/local
+export SAGE_ROOT=$PWD
+export CFLAGS="$CFLAGS -I/usr/include"
+export CXXFLAGS="$CXXFLAGS -I/usr/include"
+export CPPFLAGS="$CPPFLAGS -I/usr/include"
 ls $SAGE_ROOT
 ls $SAGE_LOCAL
 rm "$SAGE_LOCAL/lib/sage-current-location.txt"
 
 (sage -tp --initial --all --optional=memlimit,sage | tee doctest.txt) || true
 
-conda list '^sagelib$'
-conda list '^sage$'
 cat doctest.txt | grep '^sage -t ' | grep '#' > doctest.summary.txt || true
 echo `cat doctest.summary.txt | awk '{ print $5 }' | sort | grep -E '[0-9]+' | paste -sd+ | bc` failing individual doctests
 cat doctest.summary.txt | awk '{ print $5 }' | sort | grep -vE '[0-9]+' | sort | uniq -c || true
