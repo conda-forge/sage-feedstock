de56a18e92f7cb067718fd332e256dd447b9f72e
cxzhong <chenxin.zhong@outlook.com>
2026-03-18 23:35:26 +0800

    Fix: Use variable for Python command in build scripts for consistency

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ source $RECIPE_DIR/build-env.sh
 # --no-deps and --no-build-isolation lets us completely ignore dependencies and
 # build dependencies (which otherwise would create an isolated build env instead
 # of using the conda-forge toolchain and dependencies from the recipe.)
-python -m pip install --no-deps --no-build-isolation . -v
+"$PYTHON" -m pip install --no-deps --no-build-isolation . -v
 
 # Replace symlinks that point outside the prefix with files.
 rm $PREFIX/share/jupyter/kernels/sagemath/logo.svg
--- recipe/install-sagemath-bliss.sh
+++ recipe/install-sagemath-bliss.sh
@@ -7,4 +7,4 @@ source $RECIPE_DIR/build-env.sh
 # --no-deps and --no-build-isolation lets us completely ignore dependencies and
 # build dependencies (which otherwise want things such as sage-setup and
 # sage-conf which we do not want or need.)
-python -m pip install --no-deps --no-build-isolation pkgs/sagemath-bliss -vv
+"$PYTHON" -m pip install --no-deps --no-build-isolation pkgs/sagemath-bliss -vv
