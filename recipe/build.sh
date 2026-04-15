d1a4a63b700e5adfcfb48da65e3d6470487ecb3e
Julian Rüth <julian.rueth@fsfe.org>
2026-04-15 13:54:32 +0300

    Pass MESON_ARGS to meson invoked by pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,9 @@ source $RECIPE_DIR/build-env.sh
 # --no-deps and --no-build-isolation lets us completely ignore dependencies and
 # build dependencies (which otherwise would create an isolated build env instead
 # of using the conda-forge toolchain and dependencies from the recipe.)
-"$PYTHON" -m pip install --no-deps --no-build-isolation . -v --config-settings=setup-args=--wrap-mode=nofallback --config-settings=setup-args=--default-library=shared
+# The -Csetup-args makes sure that MESON_ARGS are passed on to the internal
+# meson (in particular the cross-compilation setup that is encoded there.)
+"$PYTHON" -m pip install --no-deps --no-build-isolation . -v --config-settings=setup-args=--wrap-mode=nofallback --config-settings=setup-args=--default-library=shared -Csetup-args=${MESON_ARGS// / -Csetup-args=}
 
 # Replace symlinks that point outside the prefix with files.
 rm $PREFIX/share/jupyter/kernels/sagemath/logo.svg
